a82b2e5ba362de7d1329a10aa3be765315721439
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-04 21:29:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    @@all << name 
   end 
   
   def self.clear_all
@@ -13,7 +13,7 @@ class Dog
   end
   
   def self.all
-    
+    puts @@all
   end 
   
 end 
